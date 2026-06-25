18c5760d7df0453dc3813e778a9114c41c37cc1d
jufrial02 <01jufrial@gmail.com>
2026-06-26 02:08:44 +0800

    Add player movement on click event

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("Dunia Juf dimulai...");
+
+const player = document.getElementById("player");
+
+let x = window.innerWidth / 2;
+let y = window.innerHeight / 2;
+
+document.addEventListener("click", () => {
+    x += 20;
+    player.style.left = x + "px";
+});
