a4a0c4bad2a5d47385076c15197868c80c9579e1
jufrial02 <01jufrial@gmail.com>
2026-06-26 02:56:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,20 +5,23 @@ const player = document.getElementById("player");
 let x = window.innerWidth / 2;
 let y = window.innerHeight / 2;
 
-document.addEventListener("click", () => {
-    x += 20;
-    player.style.left = x + "px";
-});
-const player = document.getElementById("player");
-
-document.addEventListener("click", (e) => {
-    player.style.left = e.clientX + "px";
-    player.style.top = e.clientY + "px";
-});
+let targetX = x;
+let targetY = y;
 
 document.addEventListener("touchstart", (e) => {
-    const touch = e.touches[0];
-
-    player.style.left = touch.clientX + "px";
-    player.style.top = touch.clientY + "px";
+    targetX = e.touches[0].clientX;
+    targetY = e.touches[0].clientY;
 });
+
+function update() {
+
+    x += (targetX - x) * 0.05;
+    y += (targetY - y) * 0.05;
+
+    player.style.left = x + "px";
+    player.style.top = y + "px";
+
+    requestAnimationFrame(update);
+}
+
+update();
