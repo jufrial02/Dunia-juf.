0e542971669c1772575a493ddedd3351312d5cc4
jufrial02 <01jufrial@gmail.com>
2026-06-26 02:50:38 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ document.addEventListener("click", () => {
     x += 20;
     player.style.left = x + "px";
 });
+const player = document.getElementById("player");
+
+document.addEventListener("click", (e) => {
+    player.style.left = e.clientX + "px";
+    player.style.top = e.clientY + "px";
+});
+
+document.addEventListener("touchstart", (e) => {
+    const touch = e.touches[0];
+
+    player.style.left = touch.clientX + "px";
+    player.style.top = touch.clientY + "px";
+});
